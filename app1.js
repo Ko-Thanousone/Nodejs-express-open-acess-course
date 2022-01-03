0831975ca07d85e7a8556830e230d26cb87fa198
Ko-Thanousone <91716252+Ko-Thanousone@users.noreply.github.com>
2022-01-03 21:23:28 +0700

    Add all file of project
    Add file

--- app1.js
+++ app1.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const debug = require('debug')('app');             
+const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3001;
 
 app.use(morgan('combined'));
-app.get("/", (req,res) =>{
+app.use(express.static(path.join(__dirname, "/public")))//link to dinamic file in public folder
+
+app.get("/", (req, res) => {
     res.send('Hello I am lerning Nodejs');
 })
-app.listen(port, ()=>{
-    debug ("Listening on port ",port);
+
+app.listen(port, () => {
+    debug("Listening on port ", port);
 })
